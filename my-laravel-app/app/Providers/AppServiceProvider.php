723719fbfddcc15055545b655c960b2a159a298c
satoru-kakinohana <satoru-kakinohana@astec-japan.co.jp>
2020-04-30 13:35:08 +0900

    add comment

--- my-laravel-app/app/Providers/AppServiceProvider.php
+++ my-laravel-app/app/Providers/AppServiceProvider.php
@@ -13,10 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-        if (\DB::getDriverName() === 'sqlite') {
-            \DB::statement(\DB::raw('PRAGMA foreign_keys=1'));
-        }
+        ////-----< For SQLite >-----
+        // if (\DB::getDriverName() === 'sqlite') {
+        //     \DB::statement(\DB::raw('PRAGMA foreign_keys=1'));
+        // }
     }
 
     /**
