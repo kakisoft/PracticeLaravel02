29d5792da99e55344d91ce518880f5aa3baf9469
satoru-kakinohana <satoru-kakinohana@astec-japan.co.jp>
2020-05-27 11:31:04 +0900

    replace " to '

--- my-laravel-app/resources/views/question01/input_cleared_users_infomation.blade.php
+++ my-laravel-app/resources/views/question01/input_cleared_users_infomation.blade.php
@@ -4,32 +4,32 @@
 
 @section('content')
 
-  <div class="page-header">
+  <div class='page-header'>
     <h2>Wow! You must be a great software engineer!</h2>
     <h2>Please leave your comment.</h2>
   </div>
 
   <hr>
 
-  <div class="form-horizontal" role="form">
-    <form method="post" accept-charset="UTF-8" action="{{action('Question01Controller@reflectClearedUserInputData')}}">
+  <div class='form-horizontal' role='form'>
+    <form method='post' accept-charset='UTF-8' action='{{action('Question01Controller@reflectClearedUserInputData')}}'>
     {{ csrf_field() }}
     {{ method_field('patch') }}
 
-      <div class="form-group">
-        <label for="user_name">Name</label>
-        <input class="form-control" type="text" value="default_name" name="user[name]" id="user_name" />
+      <div class='form-group'>
+        <label for='user_name'>Name</label>
+        <input class='form-control' type='text' value='default_name' name='user[name]' id='user_name' />
       </div>
       <br>
 
-      <div class="form-group"><label for="user_comment">Comment</label>
-        <textarea class="form-control" name="user[comment]" id="user_comment">
+      <div class='form-group'><label for='user_comment'>Comment</label>
+        <textarea class='form-control' name='user[comment]' id='user_comment'>
         </textarea>
       </div>
 
       <br>
       <div>
-        <input type="submit" name="commit" value="Submit" class="btn btn-success" />
+        <input type='submit' name='commit' value='Submit' class='btn btn-success' />
       </div>
     </form>
   </div>
