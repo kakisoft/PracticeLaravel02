bec776c5fe89382b11255cb9dcd4f21c576e22b5
kakisoft <kakisttab@gmail.com>
2020-05-31 12:34:17 +0900

    edit pagenate

--- my-laravel-app/resources/views/question01/winners.blade.php
+++ my-laravel-app/resources/views/question01/winners.blade.php
@@ -10,6 +10,11 @@
   </div>
 @endif
 
+<div>
+    <a href="{{ action('Question01Controller@index') }}" class="header-menu">Back</a>
+</div>
+<hr>
+
 <div style="text-align: center;">
     <h2>The Winners</h2>
     <h3>Let's share your winning</h3>
@@ -24,27 +29,27 @@
     </div>
     </div>
 <hr>
-    {{$cleared_users->links()}}
 
 <div>
-    <div>
+    <div style="text-align: center;">
         <table>
-        <tr>
-            <th>No.</th>
-            <th>Date</th>
-            <th>Name</th>
-            <th>Comment</th>
-        </tr>
-
-        @foreach ($cleared_users as $cleared_user)
             <tr>
-            <td>{{ $cleared_user->id }}</td>
-            <td>{{ $cleared_user->created_at }}</td>
-            <td>{{ $cleared_user->name }}</td>
-            <td>{{ $cleared_user->comment }}</td>
+                <th>No.</th>
+                <th>Date</th>
+                <th>Name</th>
+                <th>Comment</th>
             </tr>
-        @endforeach
+
+            @foreach ($cleared_users as $cleared_user)
+                <tr>
+                <td>{{ $cleared_user->id }}</td>
+                <td>{{ $cleared_user->created_at }}</td>
+                <td>{{ $cleared_user->name }}</td>
+                <td>{{ $cleared_user->comment }}</td>
+                </tr>
+            @endforeach
         </table>
+        {{$cleared_users->links()}}
     </div>
 </div>
 
