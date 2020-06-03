c16a658d3440a95946b339ab45310c01d2be4f70
satoru-kakinohana <satoru-kakinohana@astec-japan.co.jp>
2020-06-03 15:47:45 +0900

    edit routeing info

--- my-laravel-app/routes/api.php
+++ my-laravel-app/routes/api.php
@@ -28,3 +28,28 @@ Route::group(['namespace' => 'API'], function () {
     Route::get('challenge_users', 'Question01ApiController@challenge_usersGet');
     Route::post('challenge_users', 'Question01ApiController@challenge_usersPost');
 });
+
+
+// 先頭「api」をまとめて書き、先頭にプレフィックスを付ける場合、こんな感じ。
+Route::group(['namespace' => 'API'], function () {
+    //--------------------------------
+    //         Question 01
+    //--------------------------------
+    Route::prefix('q01')->group(function () {
+        Route::get('call/me', 'Question01ApiController@callMeGet');
+        Route::post('call/me', 'Question01ApiController@callMePost');
+        Route::get('challenge_users', 'Question01ApiController@challenge_usersGet');
+        Route::post('challenge_users', 'Question01ApiController@challenge_usersPost');
+    });
+
+    // //--------------------------------
+    // //         Question 02
+    // //--------------------------------
+    // Route::prefix('q02')->group(function () {
+    //     Route::get('call/me', 'Question02ApiController@callMeGet');
+    //     Route::post('call/me', 'Question02ApiController@callMePost');
+    //     Route::get('challenge_users', 'Question02ApiController@challenge_usersGet');
+    //     Route::post('challenge_users', 'Question02ApiController@challenge_usersPost');
+    // });
+
+});
