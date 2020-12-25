e4e79e8df6ab3562a1c8e641598dd68213fd2ca3
satoru_kakinohana <satoru_kakinohana@bwave.co.jp>
2020-12-25 18:51:17 +0900

    c

--- my-laravel-app/app/Http/Controllers/WebSample01Controller.php
+++ my-laravel-app/app/Http/Controllers/WebSample01Controller.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Comment;
 
 class WebSample01Controller extends Controller
 {
+    private $model_post    = Post::class;
+    private $model_comment = Comment::class;
+
+
+    public function __construct()
+    {
+    }
+
     /**
      *
      *
      */
     public function index(Request $request) {
         // $data = $this->comment->toArray();
-
+        $this->sampleMethod($request);
 
         return 999;
     }
@@ -23,6 +33,27 @@ class WebSample01Controller extends Controller
      *
      */
     public function sampleMethod($params) {
+        $params['title'] = "title01";
+
+
+        $query = Post::query();
+        $query->where('title', $params['title']);
+        $query->with(['comment' => function ($q) {
+            $q->select('post_id','body');
+        }]);
+        $query->select('id', 'title', 'body');
+
+
+
+        $records = $query->get();
+
+echo "<pre>";
+// var_dump($records->toArray());
+print_r($records->toArray());
+echo "</pre>";
+
+dump($records->toArray());
+
         return;
     }
 
